94a7693fef9f119829a9a7f2fcd63f0aaa68edfe
realglitch <60304029+realglitch@users.noreply.github.com>
2020-02-02 21:10:43 -0800

    Todos done and not done

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <h1>Create an event, {{ userName }}</h1>
+    <h1>Create an event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   computed: {
-    userName() {
-      return this.$store.state.user.name
+    catLength() {
+      return this.$store.getters.catLength
     },
-    userID() {
-      return this.$store.state.user.id
-    }
+    ...mapState(['user', 'categories'])
   }
 }
 </script>
